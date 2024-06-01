bab126e92029120db1ce3524e21676d67255ce30
Veerapat Sintupong <114942263+0622758452ab@users.noreply.github.com>
2024-06-01 13:27:55 +0700

    webpack

--- next.config.mjs
+++ next.config.mjs
@@ -18,17 +18,17 @@ const nextConfig = {
             }
         ],
     },
-    // webpack: (config, { isServer }) => { 
-    //     if (!isServer) {
-    //         config.resolve = {
-    //             ...config.resolve,
-    //             fallback: {
-    //                 fs: false,
-    //             },
-    //         };
-    //     }
-    //     return config;
-    // },
+    webpack: (config, { isServer }) => { 
+        if (!isServer) {
+            config.resolve = {
+                ...config.resolve,
+                fallback: {
+                    fs: false,
+                },
+            };
+        }
+        return config;
+    },
 };
 
 export default nextConfig;
